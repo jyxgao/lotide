6f35f7e6c9abb03f3f27331d679417ffdc50cd6b
Jenny Gao <gao.jenny9@gmail.com>
2020-07-21 02:55:03 +0000

    added new test cases

--- head.js
+++ head.js
@@ -18,3 +18,5 @@ const head = function(array) {
 
 console.log(assertEqual(head([5, 6, 7]), 5));
 console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(head(["Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(head([], undefined)));
