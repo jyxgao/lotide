a453b1c190fc5a194e6100105d7e6d2c95f1d7eb
Jenny Gao <gao.jenny9@gmail.com>
2020-07-21 02:25:22 +0000

    Created function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+/* Function that compares two input values on whether they are identical */
+
+const assertEqual = function(actual, expected) {
+  let output = "";
+  if (actual === expected) {
+    output = "🐣🐣🐣 Assertion Passed: " + actual + " === " + expected;
+  } else {
+    output = "⛔⛔⛔ Assertion Failed: " + actual + " !== " + expected;
+  }
+  return output;
+};
+
+//TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("Rubber ducks", "Rubber ducks"));
+console.log(assertEqual(3, 5));
