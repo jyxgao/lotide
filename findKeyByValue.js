71d799e1416508183ca6d3b389d04e8f1f3b6a6f
Jenny Gao <gao.jenny9@gmail.com>
2020-07-23 01:22:41 +0000

    added function

---
+++ findKeyByValue.js
@@ -0,0 +1,44 @@
+/* Function that compares two input values on whether they are identical */
+const assertEqual = function(actual, expected) {
+  let output = "";
+  if (actual === expected) {
+    console.log(output = `🐣🐣🐣 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(output = `⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return output;
+};
+
+/* Function that finds a key by value in an object */
+const findKeyByValue = function(obj, val) {
+  let keyArray = Object.keys(obj);
+  
+  //loop through all the keys in the input object
+  for (let key of keyArray) {
+    //look for a value that matches val
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+// findKeyByValue(bestTVShowsByGenre, "The Wire")
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const myFavoriteFoods = {
+  fruit: "Cherries",
+  snack: "Cuttlefish Chips",
+  protein:  "Salmon Sashimi",
+  vegetable: "Garlic Scape",
+  prepared: "Ramen"
+};
+
+assertEqual(findKeyByValue(myFavoriteFoods, "Garlic Scape"), "vegetable");
+assertEqual(findKeyByValue(myFavoriteFoods, "Ramen"), "prepared");
+assertEqual(findKeyByValue(myFavoriteFoods, "Curry Sausage"), undefined);
