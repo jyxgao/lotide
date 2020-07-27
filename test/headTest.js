3d7ba4e8fb256ad49c90d594f0859b4ff3c32a4e
Jenny Gao <gao.jenny9@gmail.com>
2020-07-27 21:10:33 +0000

    added mocha test to headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,22 @@
-const head = require('../head.js');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-//TEST CODE
-assertEqual(head([5, 6, 7]), 5); // pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // pass
-assertEqual(head(["Lighthouse", "Labs"]), "Hello"); // failed
-assertEqual(head([], undefined)); // pass
+const head = require('../head');
+
+describe('#head', () => {
+  it("should return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("should return '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("should return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  });
+
+  it("should return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
